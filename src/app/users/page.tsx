797405d714dd55a09533579d474a7eda4e527486
James Davies <jimdavies72@googlemail.com>
2024-08-05 17:42:51 +0100

    first commit

---
+++ src/app/users/page.tsx
@@ -0,0 +1,29 @@
+import {User, columns} from "./columns";
+import { DataTable } from "@/components/data-table";
+
+export const getUsers = async (): Promise<User[]> => {
+  const res = await fetch(
+    "https://64a6f5fc096b3f0fcc80e3fa.mockapi.io/api/users"
+  );
+  const data = await res.json();
+  return data;
+}
+
+
+const UserPage = async () => {
+  const data = await getUsers();
+
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="mb-6 text-3xl font-bold">All Users</h1>
+        <DataTable 
+          columns={columns} 
+          data={data} 
+        />
+      </div>
+    </section>
+  );
+};
+
+export default UserPage;
